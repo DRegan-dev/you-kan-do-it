ed5640b652b8a75168a9955b1a19fadc3ead2543
Damien Regan <damienreganmusic@gmail.com>
2024-07-24 16:09:14 +0000

    Add event listeners to each board option

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+document.getElementById('size-one'),addEventListener('click', function() {
+    createBoard(1);
+});
+
+document.getElementById('size-two'),addEventListener('click', function() {
+    createBoard(2);
+});
+
+document.getElementById('size-three'),addEventListener('click', function() {
+    createBoard(3);
+});
+
+document.getElementById('size-four'),addEventListener('click', function() {
+    createBoard(4);
+});
+
+document.getElementById('size-five'),addEventListener('click', function() {
+    createBoard(5);
+});
+
