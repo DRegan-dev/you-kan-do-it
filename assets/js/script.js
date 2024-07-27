69138384c758a13d1036f2acad5793e0279964f0
Damien Regan <damienreganmusic@gmail.com>
2024-07-27 11:43:51 +0000

    Add styles for add task form in kanban to do column

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.getElementById('size-one').addEventListener('click', function() {
     if (true) {
         const kanbanBoard = document.createElement('div');
         kanbanBoard.id = 'kanban-board-one';
-        kanbanBoard.innerHTML = '<div class="board-one-kanban-column"><h2 id="Reminders">Reminders</h2></div>';
+        kanbanBoard.innerHTML = '<div class="board-one-kanban-column"><h2 id="Reminders">Reminders</h2> <form id="to-do-form"><input type="text" placeholder="Enter Task here" id="task-input"/></form> </div>';
         document.getElementById('boardcontainer').style.display = 'flex';
         document.getElementById('boardcontainer').innerHTML = '';
         document.getElementById('boardcontainer').appendChild(kanbanBoard);
@@ -18,7 +18,7 @@ document.getElementById('size-two').addEventListener('click', function() {
     if (true) {
         const kanbanBoard = document.createElement('div');
         kanbanBoard.id = 'kanban-board-two';
-        kanbanBoard.innerHTML = '<div class="board-two-kanban-column"><h2 class="to-do-list">To Do</h2></div> <div class="board-two-kanban-column"><h2 class = "done-list">Done</h2></div>';
+        kanbanBoard.innerHTML = '<div class="board-two-kanban-column"><h2 class="to-do-list">To Do</h2> <form id="to-do-form"><input type="text" placeholder="Enter Task here" id="task-input"/></form> </div> <div class="board-two-kanban-column"><h2 class = "done-list">Done</h2></div>';
         document.getElementById('boardcontainer').style.display = 'flex';
         document.getElementById('boardcontainer').innerHTML = '';
         document.getElementById('boardcontainer').appendChild(kanbanBoard);
@@ -32,7 +32,7 @@ document.getElementById('size-three').addEventListener('click', function() {
     if (true) {
         const kanbanBoard = document.createElement('div');
             kanbanBoard.id = 'kanban-board-three';
-            kanbanBoard.innerHTML = '<div class="board-three-kanban-column"><h2 class="to-do-list">To Do</h2></div> <div class="board-three-kanban-column"><h2 class = "in-progess-list">In Progress</h2></div> <div class="board-three-kanban-column"><h2 class="done-list">Done</h2></div>';
+            kanbanBoard.innerHTML = '<div class="board-three-kanban-column"> <h2 class="to-do-list">To Do</h2> <form id="to-do-form"><input type="text" placeholder="Enter Task here" id="task-input"/> <button type="submit">ADD</button></form> </div> <div class="board-three-kanban-column"><h2 class = "in-progess-list">In Progress</h2></div> <div class="board-three-kanban-column"><h2 class="done-list">Done</h2></div>';
             document.getElementById('boardcontainer').style.display = 'flex';
             document.getElementById('boardcontainer').innerHTML = '';
             document.getElementById('boardcontainer').appendChild(kanbanBoard);
@@ -41,33 +41,6 @@ document.getElementById('size-three').addEventListener('click', function() {
     }
 });
 
-document.getElementById('size-four').addEventListener('click', function() {
-    console.log("Clicked size four");
-    if (true) {
-        const kanbanBoard = document.createElement('div');
-            kanbanBoard.id = 'kanban-board-four';
-            kanbanBoard.innerHTML = '<div class="board-four-kanban-column">Kanban Column</div> <div class="board-four-kanban-column">Kanban Column 2</div> <div class="board-four-kanban-column">Kanban Column 3</div> <div class="board-four-kanban-column">Kanban Column 4</div>';
-            document.getElementById('boardcontainer').style.display = 'flex';
-            document.getElementById('boardcontainer').innerHTML = '';
-            document.getElementById('boardcontainer').appendChild(kanbanBoard);
-    } else {
-        console.log("The board size has not been selected.")
-    }
-});
-
-document.getElementById('size-five').addEventListener('click', function() {
-    console.log("Clicked size five");
-    if (true) {
-        const kanbanBoard = document.createElement('div');
-            kanbanBoard.id = 'kanban-board-five';
-            kanbanBoard.innerHTML = '<div class="board-five-kanban-column">Kanban Column</div> <div class="board-five-kanban-column">Kanban Column 2</div> <div class="board-five-kanban-column">Kanban Column 3</div> <div class="board-five-kanban-column">Kanban Column 4</div> <div class="board-five-kanban-column">Kanban Column 5</div>';
-            document.getElementById('boardcontainer').style.display = 'flex';
-            document.getElementById('boardcontainer').innerHTML = '';
-            document.getElementById('boardcontainer').appendChild(kanbanBoard);
-    } else {
-        console.log("The board size has not been selected.")
-    }
-});
 
 
 // function createBoard(numColumns) {
