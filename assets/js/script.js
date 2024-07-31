12b178f91cf569faad6a386d33d1065b3aa61956
Damien Regan <damienreganmusic@gmail.com>
2024-07-31 16:40:33 +0000

    Refactor code for enableNewTask function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,87 +10,90 @@
 //         console.log("Hello World!")
 //     }
 // });
-function enableNewTask(kanbanBoardId) {
+
+function enableNewTask() {
     const newTaskInput = document.getElementById("new-task-input");
+    if (!newTaskInput) {
+        console.log("Element 'new-task-input' not found")
+        return;
+    }
 
-    newTaskInput.addEventListener("keyup", function(event)) {
+    const kanbanBoardOne = document.getElementById("kanban-board-one");
+    if (!kanbanBoardOne) {
+        console.log("Element 'kanban-board-one' not found.")
+        return;
+    }
+
+    const kanbanBoardTwo = document.getElementById("kanban-board-two");
+    if (!kanbanBoardTwo) {
+        console.log("Element 'kanban-board-two' not found.")
+        return;
+    }
+
+    const kanbanBoardThree = document.getElementById("kanban-board-three");
+    if (!kanbanBoardThree) {
+        console.log("Element 'kanban-board-three' not found.")
+        return;
+    }
+
+    const addTask = document.getElementById("board-one-todo");
+    if (!addTask) {
+        console.log("Element 'board-one-todo' not found.")
+    }
+
+    newTaskInput.addEventListener("keyup", function(event) {
         if (event.key === "Enter") {
-            const taskValue = event.target.value;
-
+            const taskValue = event.target.value.trim();
+            if (!taskValue) {
+                console.log("Empty task connot be added.")
+                return;
+            }
             const newTask = document.createElement('p');
             newTask.classList.add("task");
             newTask.innerText = taskValue;
-            
-            let targetColumn; 
-            if(kanbanBoardId === "kanban-board-one") {
-                targetColumn = document.getElementById("board-one-todo");
-            } else if (kanbanBoardId === "kanban-board-two") {
-                targetColumn = document.getElementById("board-two-todo")
-            } else if (kanbanBoardId === "kanban-board-three") {
-                targetColumn = document.getElementById("board-three-todo")
-            } else {
-                console.log("Invalid kanban board ID")
-            }
-              
-
-            kanbanColumnOne.appendChild(createTaskElement());
-            kanbanColumnTwo.appendChild(createTaskElement());
-            kanbanColumnThree.appendChild(createTaskElement());
-            console.log(`newTask ${taskValue}`);
-            event.target.value = "";
-
-            } else {
-                console.log("Empty Task Cannot be added.")
-            }
+            addTask.appendChild(newTask);
+            console.log(`New Task added: ${taskValue}`);
         }
     });
 }
 
 
+
+
 console.log("Hello World!")
 document.getElementById('size-one').addEventListener('click', function() {
     console.log("Clicked size one");
-    if (true) {
-        const kanbanBoard = document.createElement('div');
-        kanbanBoard.id = 'kanban-board-one';
-        kanbanBoard.innerHTML = '<div class="board-one-kanban-column" id="board-one-todo"><h2 id="Reminders">Reminders</h2> <input type="text" placeholder="Enter Task here" id="new-task-input"/> </div>';
-        document.getElementById('boardcontainer').style.display = 'flex';
-        document.getElementById('boardcontainer').innerHTML = '';
-        document.getElementById('boardcontainer').appendChild(kanbanBoard);
-        enableNewTask();
-    } else {
-        console.log("The board size has not been selected.")
-    }
+    const kanbanBoard = document.createElement('div');
+    kanbanBoard.id = 'kanban-board-one';
+    kanbanBoard.innerHTML = '<div class="board-one-kanban-column" id="board-one-todo"><h2 id="Reminders">Reminders</h2> <input type="text" placeholder="Enter Task here" id="new-task-input"/> </div>';
+    document.getElementById('boardcontainer').style.display = 'flex';
+    document.getElementById('boardcontainer').innerHTML = '';
+    document.getElementById('boardcontainer').appendChild(kanbanBoard);
+    enableNewTask();
 });
 
 document.getElementById('size-two').addEventListener('click', function() {
     console.log("Clicked size two");
-    if (true) {
-        const kanbanBoard = document.createElement('div');
-        kanbanBoard.id = 'kanban-board-two';
-        kanbanBoard.innerHTML = '<div class="board-two-kanban-column" id="board-two-todo"><h2 class="to-do-list">To Do</h2> <input type="text" placeholder="Enter Task here" id="new-task-input"/> </div> <div class="board-two-kanban-column"><h2 class = "done-list">Done</h2></div>';
-        document.getElementById('boardcontainer').style.display = 'flex';
-        document.getElementById('boardcontainer').innerHTML = '';
-        document.getElementById('boardcontainer').appendChild(kanbanBoard);
-        enableNewTask()
-    } else {
-        console.log("The board size has not been selected.")
-    }
+
+    const kanbanBoard = document.createElement('div');
+    kanbanBoard.id = 'kanban-board-two';
+    kanbanBoard.innerHTML = '<div class="board-two-kanban-column" id="board-two-todo"><h2 class="to-do-list">To Do</h2> <input type="text" placeholder="Enter Task here" id="new-task-input"/> </div> <div class="board-two-kanban-column"><h2 class = "done-list">Done</h2></div>';
+    document.getElementById('boardcontainer').style.display = 'flex';
+    document.getElementById('boardcontainer').innerHTML = '';
+    document.getElementById('boardcontainer').appendChild(kanbanBoard);
+    enableNewTask()
 });
 
 document.getElementById('size-three').addEventListener('click', function() {
     console.log("Clicked size three");
-    if (true) {
-        const kanbanBoard = document.createElement('div');
-            kanbanBoard.id = 'kanban-board-three';
-            kanbanBoard.innerHTML = '<div class="board-three-kanban-column" id="board-three-todo"> <h2 class="to-do-list">To Do</h2> <input type="text" placeholder="Enter Task here" id="new-task-input"/> </div> <div class="board-three-kanban-column"><h2 class = "in-progess-list">In Progress</h2></div> <div class="board-three-kanban-column"><h2 class="done-list">Done</h2></div>';
-            document.getElementById('boardcontainer').style.display = 'flex';
-            document.getElementById('boardcontainer').innerHTML = '';
-            document.getElementById('boardcontainer').appendChild(kanbanBoard);
-            enableNewTask()
-    } else {
-        console.log("The board size has not been selected.")
-    }
+    
+    const kanbanBoard = document.createElement('div');
+    kanbanBoard.id = 'kanban-board-three';
+    kanbanBoard.innerHTML = '<div class="board-three-kanban-column" id="board-three-todo"> <h2 class="to-do-list">To Do</h2> <input type="text" placeholder="Enter Task here" id="new-task-input"/> </div> <div class="board-three-kanban-column"><h2 class = "in-progess-list">In Progress</h2></div> <div class="board-three-kanban-column"><h2 class="done-list">Done</h2></div>';
+    document.getElementById('boardcontainer').style.display = 'flex';
+    document.getElementById('boardcontainer').innerHTML = '';
+    document.getElementById('boardcontainer').appendChild(kanbanBoard);
+    enableNewTask()
 });
 
 
